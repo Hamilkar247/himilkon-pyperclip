a05f883d030a4e43a72f56549a077b4f5d4de9c4
Hamilkar247 <adonnibal96@gmail.com>
2020-10-23 14:35:55 +0200

    ISS-9 listuje wszystkie organizacje jak i konkretne rekordy

--- wagalogitech/front/urls.py
+++ wagalogitech/front/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import FrontViewSet
-
-router = DefaultRouter()
-router.register(r'main', views.FrontViewSet)
+from . import views
+#router = DefaultRouter()
+#router.register(r'main', views.FrontViewSet)
 
 urlpatterns = [
-    path('front/', include(router.urls)),
+    path('front_root', views.front_root, name='front_root'),
+    path('login', views.login, name="login"),
+    path('organizacje', views.organizacje, name="organizacje"),
+    path('organizacje/<int:organizacja_id>', views.szczegoly, name="organizacje")
 ]
--- wagalogitech/front/views.py
+++ wagalogitech/front/views.py
@@ -1,8 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from api.models import Organizacja
 
 # Create your views here.
 from requests import Response
 
 
-def front_root(request, format=None):
-    return Response("Dzieńdobry")
+def front_root(request):
+    return HttpResponse("Waga - LOGITECH")
+
+
+def login(request):
+    return HttpResponse("Login")
+
+
+def organizacje(request):
+    query_set=Organizacja.objects.all()
+    output=', '.join([q.nazwa for q in query_set])
+    return HttpResponse("Organizacje:"+output)
+
+
+def szczegoly(request, organizacja_id):
+    query_set=Organizacja.objects.get(pk=organizacja_id)
+    output="Nazwa:" + str(query_set.nazwa) + " Opis:"+ str(query_set.opis)
+    return HttpResponse(output)
