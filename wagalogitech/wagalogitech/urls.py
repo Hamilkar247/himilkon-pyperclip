c9171a502a6788608ec88a46518c43d2b8fcbd3a
Hamilkar247 <adonnibal96@gmail.com>
2020-10-26 13:36:04 +0100

    ISS-11 ekran logowania - wstepny

--- wagalogitech/wagalogitech/urls.py
+++ wagalogitech/wagalogitech/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('front/', include('front.urls'))
+    path('front/', include('front.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
