f1787aaee3557f8f2387696629e3cf147cf2e2c8
Hamilkar247 <adonnibal96@gmail.com>
2020-11-03 11:05:46 +0100

    ISS-6 przeniesienie admin.py z API do CORE

--- wagalogitech/core/admin.py
+++ wagalogitech/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Organizacja, SesjaUzytkownika, \
+    LogAdministracyjny, SeriaPomiarowa, Pomiar, LogPomiarowy
+
+admin.site.register(Organizacja)
+#admin.site.register(User)
+admin.site.register(SesjaUzytkownika)
+admin.site.register(LogAdministracyjny)
+admin.site.register(SeriaPomiarowa)
+admin.site.register(Pomiar)
+admin.site.register(LogPomiarowy)
