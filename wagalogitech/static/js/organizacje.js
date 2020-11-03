fd236fdadfb41e0c473f374bb1ed0e531a13ea1d
Hamilkar247 <adonnibal96@gmail.com>
2020-11-03 14:03:48 +0100

    ISS-28 testowa funkcja w jquery

--- wagalogitech/static/js/organizacje.js
+++ wagalogitech/static/js/organizacje.js
@@ -1,7 +1,15 @@
 function changeParagraph() {
+  console.log("Juzue")
   document.getElementById("paragraphId").innerHTML = "static/js/Paragraph changed.";
 }
 
 function refreshPage() {
   window.location.reload();
 }
+
+//testowa funkcja jquery
+$(document).ready(function(){
+  $("#hideclick").click(function(){
+    $(this).hide();
+  })
+})
