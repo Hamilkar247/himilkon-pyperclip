271085ad232632e499b8b45ee2793ecd3d6b42e6
Hamilkar247 <adonnibal96@gmail.com>
2020-11-17 13:04:32 +0100

    ISS-44 pomiary.js  i pomiary modyfikacja

--- wagalogitech/static/js/pomiary.js
+++ wagalogitech/static/js/pomiary.js
@@ -14,9 +14,11 @@ $(document).ready(function(){
               '<td>' + response[i].czyWazny + '</td>' +
               '<td>' + response[i].wartosc + '</td>' +
               '<td>' + response[i].data_pomiaru + '</td>' +
-              '<form action="/front/pomiary/"' + response[i].id + '" >' +
-              '<input class="btn btn-primary btn-sm" type="submit" + value=' + response[i].nazwa + '/>' +
-              '</form>' +
+              '<td>' +
+                 '  <a href="/front/pomiary/' + response[i].id + '" class="btn btn-primary btn-sm">' + response[i].id + '</a>' +
+                 '  <form action="/front/pomiary/"' + response[i].id + '" >' +
+                 '     <input class="btn btn-primary btn-sm" type="submit" + value=' + response[i].id + '/>' +
+                 '  </form>' +
               '</td>' +
            '</tr>');
       }
