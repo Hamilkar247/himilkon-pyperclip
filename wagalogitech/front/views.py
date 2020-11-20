41be4af9a433e478446766d7430484063cc4603c
Hamilkar247 <adonnibal96@gmail.com>
2020-11-20 11:39:38 +0100

    ISS-49 organizacja_nowy i związane z tym kwestie

--- wagalogitech/front/views.py
+++ wagalogitech/front/views.py
@@ -33,6 +33,10 @@ def organizacja_detail(request, organizacja_id):
     return render(request, "front/organizacja_detail.html", {'organizacja': organizacja})
 
 
+def organizacje_nowy(request):
+    return render(request, "front/organizacje_nowy.html")
+
+
 def pomiary(request):
     pomiary_list = Pomiar.objects.all()
     template = loader.get_template('front/pomiary.html')
