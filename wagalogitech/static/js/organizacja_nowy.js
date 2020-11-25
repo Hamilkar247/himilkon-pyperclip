89e7a076a2c3d313c65710ef93a8ca0316b2a13b
Hamilkar247 <adonnibal96@gmail.com>
2020-11-25 15:57:34 +0100

    ISS-49 wywolanie ajax funkcji po nacisnieciu zapisz organizcja - uwaga button musi byc typu submit a formularz zalecany taki jak wyżej
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- wagalogitech/static/js/organizacja_nowy.js
+++ wagalogitech/static/js/organizacja_nowy.js
@@ -1,23 +1,41 @@
 console.log("organizacja_nowy.js")
 
 
-$('.ZapiszOrganizacje').click( function() {
-   console.log("console log - organizacja nowy")
-   $.ajax({
-      url        : "http://127.0.0.1:8000/api/organizacje",
-      method     : "POST",
-      success : function (data){
-               $('#nazwa_organizacji').text(data[0].nazwa);
-               $('#opis_organizacji'.text(data[0].opis));
-      }
-//      data       : {
-//          "nazwa"   : "Fundacja gustawa adolfa",
-//          "opis"    : "pulawska 3a"
+//$('#ZapiszOrganizacje').on("click", function(e) {
+//   console.log("console log - organizacja nowy")
+//});
+
+//wyswietlanie danych
+$("#form_zapisorg").on("submit", function(e) {
+   e.preventDefault()
+   console.log("form_zapis_org")
+   var wyswietl=document.getElementById("opis_organizacji").value
+   console.log("wyswietl:" +wyswietl )
+   var wyswietl2=$("#opis_organizacji").val()
+   console.log("wyswietl2: " +wyswietl2)
+})
+//   $.ajax({
+//      url        : "http://127.0.0.1:8000/api/organizacje",
+//      type       : "POST",
+//      //beforeSend: function(xhr, settings) {
+//      //    xhr.setRequestHeader("X-CSRFToken", '{{ csrf_token }}');
+//      //},
+//      //success: function (arg){
+//      //     location.reload()
+//      //}
+//      //dataType   : "json",
+//      //success    : function (data){
+//      //         $('#nazwa_organizacji').text(data[0].nazwa);
+//      //         $('#opis_organizacji'.text(data[0].opis));
+//      //}
+//      data: {
+//             nazwa: $("#nazwa_organizacji").val(),
+//             opis: $("#opis_organizacji").val(),
 //      }
-//   }).done( function (response){
-//       alert("Zapisano organizacje")
-//   }).fail( function(){
-//       alert("Organizacja nie przeszła")
-//   })
-   });
-});
+//      success: function(){
+//         alert("Dodano organizacje successfully!")
+//      }
+//      error: function(){
+//         alert("Nie udalo sie dodac organizacji :'(")
+//      }
+//   });
