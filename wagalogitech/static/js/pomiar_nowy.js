29e2d5f51071f90a40b58e3ef60ab78f5919a437
Hamilkar247 <adonnibal96@gmail.com>
2020-12-03 14:26:18 +0100

    ISS-72 pomiary - działające wrzucanie POST korzystając z api - występuje niestety nadal blad 500 przy wysylaniu danych
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- wagalogitech/static/js/pomiar_nowy.js
+++ wagalogitech/static/js/pomiar_nowy.js
@@ -1,5 +1,20 @@
 console.log("pomiar_nowy.js")
-
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
 let start_owner=''
 let start_czyWazny=''
 let start_wartosc=''
@@ -11,40 +26,40 @@ $("#form_zapis_pomiar").on("submit", function(e) {
    // efekt jest taki ze wywolanie submit nie skutkuje przeladowaniem strony, ktorym to submit domyslnie skutkuje
    e.preventDefault()
    console.log("form_zapis_pomiar")
-   //Inny sposób na pobieranie danych z HTML
-   //var wyswietl=document.getElementById("opis_organizacji").value
-   //console.log("wyswietl:" +wyswietl )
-   //var wyswietl2=$("#opis_organizacji").val()
-   //console.log("wyswietl2: " +wyswietl2)
 
-   var nazwa_org=$("#nazwa_organizacji").val()
-   console.log("nazwa_org: "+ nazwa_org)
-   var opis_org=$("#opis_organizacji").val()
-   console.log("opis_org: " + opis_org)
-
+   start_owner="arek" // do zmiany !
+   start_czyWazny=$("#czyWazny").val()
+   start_wartosc=$("#wartosc").val()
+   start_data_pomiaru="2020-11-04T13:06:12.757407Z"//$("#data_pomiaru").val()
+   console.log("owner: " + start_owner)
+   console.log("czyWazny: " + start_czyWazny)
+   console.log("wartosc: " + start_wartosc)
+   console.log("data_pomiaru: " + start_data_pomiaru)
    //jeszcze inny sposób tablicowy
    //var data = {};
    //data.nazwa = $("#nazwa_organizacji").val()
    //data.opis = $("#opis_organizacji").val()
 
    $.ajax({
-      url      : "http://127.0.0.1:8000/api_v1/pomiar/dodaj",
+      url      : "http://127.0.0.1:8000/api/v1/pomiary",
       type     : "POST",
       data     : {
-          nazwa: nazwa_org,
-          opis : opis_org,
-          csrfmiddlewaretoken: '{{ csrf_token }}',
+        //  owner:        start_owner,
+          czyWazny:     start_czyWazny,
+          wartosc:      start_wartosc,
+        //  data_pomiaru: start_data_pomiaru,
+          csrfmiddlewaretoken: csrftoken,
       },
-      success: function(){
-         console.log("nazwa org:"+ nazwa_org)
-         console.log("opis org:" + opis_org)
-         alert ('Save data');
-         //window.location = "/";
-      },
-      failure: function(){
-         alert ('Nie zostala dodana zmienna');
-      }
-   });
+   }).done(function(response) {
+         alert('Save data');
+   }).fail(function() {
+         alert("Wystąpił błąd w połączeniu z djangorestapi")
+   }).then(function() {
+         console.log("owner: " + start_owner);
+         console.log("czyWazny: " + start_czyWazny);
+         console.log("wartosc: " + start_wartosc);
+         console.log("data_pomiaru: " + start_data_pomiaru);
+   })
   //alternatywna wersja wyswietlania danych w zapisie poza ajaxem
   ///.done(function(response){
   ///     console.log("nazwa org:"+ nazwa)
