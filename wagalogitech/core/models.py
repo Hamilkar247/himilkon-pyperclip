795f49b879b64cbd84e9e79d3dc821b39f2e7ad6
Hamilkar247 <adonnibal96@gmail.com>
2020-12-09 14:00:58 +0100

    ISS-76 core/models.py - przemodelowanie bazy danych zgodnie z schematem
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- wagalogitech/core/models.py
+++ wagalogitech/core/models.py
@@ -3,15 +3,14 @@ from django.db import models
 
 class Pomiar(models.Model):
     czyWazny = models.BooleanField(verbose_name="Czy ważny?")
+    jednostka_miary = models.CharField(verbose_name="Jednostka miary", max_length=50)
     wartosc = models.CharField(max_length=300, verbose_name="wartość")
     data_pomiaru = models.DateTimeField(blank=False, verbose_name="data pomiaru", auto_now_add=True)
-    # sesja_uzytkownika = models.ForeignKey(SesjaUzytkownika, on_delete=models.CASCADE)
-    owner = models.ForeignKey('auth.User', related_name='pomiary', on_delete=models.CASCADE)
-
-    # highlighted = models.TextField()
+    sesja_uzytkownika = models.ForeignKey('SesjaUzytkownika', on_delete=models.CASCADE)
+    seria_pomiarowa = models.ForeignKey('SeriaPomiarowa', on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.owner.verbose_name) + ' ' + str(self.data_pomiaru) + ' wart: ' + str(self.wartosc)
+        return str(self.sesja_uzytkownika) + ' ' + str(self.seria_pomiarowa) + ' ' + str(self.data_pomiaru) + ' wart: ' + str(self.wartosc)
 
     def save(self, *args, **kwargs):
         czyWazny = 'table' if self.czyWazny else False
@@ -45,6 +44,9 @@ class Organizacja(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nazwa = models.CharField(max_length=200, verbose_name="Nazwa")
     opis = models.CharField(max_length=500, verbose_name="Opis")
+    adres = models.CharField(max_length=200, verbose_name="Adres")
+    #to bardziej szef szefow niz uzytkownik - potem trzeba pomyśleć czy nei trzeba by zrobić organizacja uzytkownik many to many
+    #owner = models.ForeignKey('auth.User', related_name='pomiary', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nazwa
@@ -59,16 +61,12 @@ class Organizacja(models.Model):
 
 
 class SesjaUzytkownika(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-    # pomiar = models.ForeignKey(Pomiar, on_delete=models.CASCADE)
-    # uzytkownik = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    # tymczasowo_dodane
     start_sesji = models.DateTimeField(blank=False, verbose_name="start sesji")
+    koniec_sesji = models.DateTimeField(blank=True, verbose_name="koniec sesji")
     owner = models.ForeignKey("auth.User", related_name='sesjeuzytkownika', on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.start_sesji)
+        return str(self.owner) + " " + str(self.start_sesji)
 
     def save(self, *args, **kwargs):
         super(SesjaUzytkownika, self).save(*args, **kwargs)
@@ -80,15 +78,11 @@ class SesjaUzytkownika(models.Model):
 
 
 class LogAdministracyjny(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     opis = models.CharField(max_length=500, verbose_name="Opis")
     data = models.DateField(blank=False, verbose_name="Data")
     czynnosc = models.CharField(max_length=200, verbose_name="Czynność")
     owner = models.ForeignKey("auth.User", related_name="logiadministracyjne", on_delete=models.CASCADE)
-
-    # uzytkownik = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE, verbose_name="Użytkownik")
-    # jednostka_organizacyjna = models.ForeignKey(JednostkaOrganizacyjna, on_delete=models.CASCADE,
-    #                                            verbose_name="Jednostka organizacyjna")
+    organizacja = models.ForeignKey(Organizacja, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Log Administracyjny"
@@ -103,31 +97,29 @@ class LogAdministracyjny(models.Model):
 
 
 class SeriaPomiarowa(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nazwa_sesji = models.CharField(max_length=100, verbose_name="nazwa sesji")
-    owner = models.ForeignKey("auth.User", related_name='seriepomiarowe', on_delete=models.CASCADE)
-
-    # jednostka_organizacyjna = models.ForeignKey(JednostkaOrganizacyjna, on_delete=models.CASCADE)
+    nazwa_serii = models.CharField(max_length=100, verbose_name="nazwa serii")
+    opis = models.CharField(max_length=300, verbose_name="opis serii")
+    czyZakonczona = models.BooleanField(verbose_name="Czy zakończono serie pomiarową?")
+    organizacja = models.ForeignKey(Organizacja, on_delete=models.CASCADE)
+    start_pomiaru = models.DateTimeField(blank=False, verbose_name="start pomiaru")
+    koniec_pomiaru = models.DateTimeField(blank=True, verbose_name="koniec pomiaru")
 
     def __str__(self):
-        return str(self.nazwa_sesji)
+        return str(self.nazwa_serii)
 
     def save(self, *args, **kwargs):
         super(SeriaPomiarowa, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name = "Seria Pomiarowwa"
+        verbose_name = "Seria Pomiarowa"
         verbose_name_plural = "Serie Pomiarowe"
 
 
-# ======= https://www.django-rest-framework.org/tutorial/1-serialization/#writing-regular-django-views-using-our
-# -serializer
 class LogPomiarowy(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     opis = models.CharField(max_length=500, verbose_name="Opis")
     data = models.DateField(verbose_name="data")
-    # pomiar = models.ForeignKey(Pomiar, on_delete=models.CASCADE)
-    owner = models.ForeignKey('auth.User', related_name="logipomiarowe", on_delete=models.CASCADE)
+    czynnosc = models.CharField(max_length=300, verbose_name="Czynność")
+    pomiar = models.ForeignKey(Pomiar, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.opis)
--- wagalogitech/core/urls.py
+++ wagalogitech/core/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.core_root, name="core_root"),
 ]
