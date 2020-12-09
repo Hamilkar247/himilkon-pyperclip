1703d5203e8fde817b1ac86bf7ff5393c00c4f32
Hamilkar247 <adonnibal96@gmail.com>
2020-12-10 00:24:22 +0100

    ISS-82 usuniecie zbędnych komentarzy i biblitoek
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- wagalogitech/api_v1/views.py
+++ wagalogitech/api_v1/views.py
@@ -1,13 +1,8 @@
 # Create your views here.
-from django.contrib.auth.models import User
-from django.http import JsonResponse, Http404
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from rest_framework import permissions, viewsets, generics, status
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework.views import APIView
-from django.shortcuts import redirect
 
 from core.models import Pomiar, LogPomiarowy, LogAdministracyjny \
     , SeriaPomiarowa, Organizacja, SesjaUzytkownika, User
@@ -30,13 +25,7 @@ def api_root(request, format=None):
     })
 
 
-# This time we've used the ModelViewSet class in order to get the complete set of default read and write operations.
 class PomiarViewSet(viewsets.ModelViewSet):
-    """
-    This viwset automatically provides 'list', 'create', 'retrieve',
-    'update' and 'destroy' actions
-    """
-
     queryset = Pomiar.objects.all()
     serializer_class = PomiarSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
@@ -55,9 +44,6 @@ class OrganizacjaViewSet(viewsets.ModelViewSet):
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    This viewsets automatically provides 'list' and 'detail' actions
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
