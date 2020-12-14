ec8cfdea65e272059058cabbd829a1256c014e07
Hamilkar247 <adonnibal96@gmail.com>
2020-12-14 13:47:21 +0100

    [ISS-89] add app_name to all apps
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- wagalogitech/api_v1_2/urls.py
+++ wagalogitech/api_v1_2/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from . import ApiV12Config
 
+apps_name = ApiV12Config
 urlpatterns = [
     path('', views.view_api_v1_2, name="api_v1_2_root"),
 ]
--- wagalogitech/core/urls.py
+++ wagalogitech/core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from . import CoreConfig
 
+app_names = CoreConfig.name
 urlpatterns = [
 ]
