ede88bf23efb92f331cc9b75ec6205c934a4616e
Hamilkar247 <adonnibal96@gmail.com>
2020-12-16 01:27:24 +0100

    WO-1 podstawowa funkcjonalnosc - url jest kopiowany z .git slesh config defacto
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- himilkon-pyperclip.py
+++ himilkon-pyperclip.py
@@ -1,13 +1,15 @@
 import logging
 import argparse
+import pyperclip as pc
+import subprocess
 
 def def_params():
     parser = argparse.ArgumentParser(
-            description="Description to fill" 
+            description="Description to fill"
     )
     parser.add_argument("-l", "--loghami", action='store_true', help="set debug")
     args = parser.parse_args()
-    if args.loghami
+    if args.loghami:
         logging.basicConfig(level=logging.DEBUG)
         print("args:" + str(args))
     return args
@@ -15,6 +17,11 @@ def def_params():
 
 def main():
     args=def_params()
+    gurl=subprocess.Popen(['git', 'config', '--get', 'remote.origin.url'], stdout=subprocess.PIPE)
+    output_gurl = gurl.stdout.read()
+    print(output_gurl)
+    pc.copy(str(output_gurl)[2:-3])
+
 
 if __name__ == "__main__":
     main()
