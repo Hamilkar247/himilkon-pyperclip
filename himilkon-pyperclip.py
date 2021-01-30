3ad9e9b2875932dd70c1aa88ee9647fd01ad4d7f
Hamilkar247 <adonnibal96@gmail.com>
2021-01-30 20:50:33 +0100

    [WO-4] gurlfunction jako parametr
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- himilkon-pyperclip.py
+++ himilkon-pyperclip.py
@@ -8,20 +8,23 @@ def def_params():
             description="Description to fill"
     )
     parser.add_argument("-l", "--loghami", action='store_true', help="set debug")
+    parser.add_argument("-gurl", "--geturl", action='store_true', help='zwraca url do repa')
     args = parser.parse_args()
     if args.loghami:
         logging.basicConfig(level=logging.DEBUG, force=True)
         print("args:" + str(args))
     return args
 
-
-def main():
-    args=def_params()
+def gurlfunction():
     gurl=subprocess.Popen(['git', 'config', '--get', 'remote.origin.url'], stdout=subprocess.PIPE)
     output_gurl = gurl.stdout.read()
     print(output_gurl)
     pc.copy(str(output_gurl)[2:-3])
 
+def main():
+    args=def_params()
+    if args.geturl:
+        gurlsfunction()
 
 if __name__ == "__main__":
     main()
